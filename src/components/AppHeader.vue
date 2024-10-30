b5bb1e63bf63f4d4a012797c99c63a5c16fdc7b9
Muhamad Faturrahman <114564154+MuhamadFaturrahman@users.noreply.github.com>
2024-10-30 22:42:32 +0700

    Adding Footer & Header Bar Section

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,5 @@
 <template>
-    <v-app-bar :elevation="2">
-  <template v-slot:prepend>
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-  </template>
-
-  <v-app-bar-title>Application Bar</v-app-bar-title>
-</v-app-bar>
+  <v-app-bar :elevation="2">
+    <v-app-bar-title>News Apps Test</v-app-bar-title>
+  </v-app-bar>
 </template>
